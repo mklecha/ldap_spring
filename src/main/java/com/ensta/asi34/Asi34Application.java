ec59a2d68d1452962bc60ca83a35e9f2898b7a01
unknown <jesusmanuel93@hotmail.com>
2018-01-30 09:47:16 +0100

    Init

---
+++ src/main/java/com/ensta/asi34/Asi34Application.java
@@ -0,0 +1,12 @@
+package com.ensta.asi34;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Asi34Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Asi34Application.class, args);
+	}
+}
