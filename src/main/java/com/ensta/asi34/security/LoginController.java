e1598970c0bf0964ae7f6518cee8d72b8c873b39
unknown <jesusmanuel93@hotmail.com>
2018-02-06 11:15:12 +0100

    Login template

--- src/main/java/com/ensta/asi34/security/LoginController.java
+++ src/main/java/com/ensta/asi34/security/LoginController.java
@@ -1,12 +1,13 @@
 package com.ensta.asi34.security;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
 
-//    @RequestMapping(path="/login")
-//    public String login(){
-//        return "login";
-//    }
+    @RequestMapping(path="/login")
+    public String login(){
+        return "login";
+    }
 }
